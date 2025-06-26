698c0f5d30d3f8fade041ca53de70c996f454f07
bhavyabin <bkrockxxx@gmail.com>
2025-06-26 18:30:36 +0530

    main

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, jsonify, abort
 from scrape import get_weather_info,get_weather_info_using_station_id
+from werkzeug.middleware.proxy_fix import ProxyFix
 from citylist import cities
 import json
 
 app = Flask(__name__, static_url_path="/s")
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 @app.route("/")
 def initiator():
